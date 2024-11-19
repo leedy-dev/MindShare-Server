066b77bdabbca1af33f3df4b8f139bcf16911a61
ledoye <76687469+ledoye@users.noreply.github.com>
2024-11-19 13:20:05 +0900

    Feat: MindShare AMS 추가

---
+++ mindshare-ams/src/main/java/com/mindshare/ams/MindshareAmsApplication.java
@@ -0,0 +1,13 @@
+package com.mindshare.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindshareAmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindshareAmsApplication.class, args);
+    }
+
+}
