161cf71fc1e0727523554883f8d88eca39fe63ab
ledoye <76687469+ledoye@users.noreply.github.com>
2024-11-19 13:19:49 +0900

    Feat: MindShare CMS 추가

---
+++ mindshare-cms/src/main/java/com/mindshare/cms/MindshareCmsApplication.java
@@ -0,0 +1,13 @@
+package com.mindshare.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindshareCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindshareCmsApplication.class, args);
+    }
+
+}
